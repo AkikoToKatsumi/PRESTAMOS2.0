406fe1716c97e197d33453d7ff2ecfb8d12b52a8
Carlos Jose Gomez <36394149+wayne1896@users.noreply.github.com>
2024-06-26 23:39:13 -0400

    Paginas
    Paginas

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -61,48 +61,48 @@ class="light-style layout-menu-fixed"
 $peticionAjax=false;
 require_once "./controladores/vistasControlador.php";
 $IV = new vistasControlador();
-  
-   $vistas = $IV-> obtener_vistas_controlador();
- //condicional para detectar login
 
-   if ($vistas=="login" || $vistas=="404"){
-  require_once "./vistas/contenidos/".$vistas."-view.php";
+ $vistas = $IV-> obtener_vistas_controlador();
+ //condicional para detectar login
+ if ($vistas=="login" || $vistas=="404"){
+    require_once "./vistas/contenidos/".$vistas."-view.php";
 
  }else{session_start(['name'=>'SPM']);
+$pagina=explode("/", $_GET['views']);
 
-      require_once"./controladores/loginControlador.php";
-      $lc = new loginControlador();
+    require_once"./controladores/loginControlador.php";
+    $lc = new loginControlador();
 
-      if(!isset($_SESSION['token_spm']) || !isset($_SESSION['usuario_spm']) ||
-      !isset($_SESSION['privilegio_spm']) || !isset($_SESSION['id_spm']) ){
+    if(!isset($_SESSION['token_spm']) || !isset($_SESSION['usuario_spm']) ||
+    !isset($_SESSION['privilegio_spm']) || !isset($_SESSION['id_spm']) ){
 echo $lc->forzar_cierre_sesion_controlador() ;
 exit();
-      }
+    }
 ?>
 
 
-		<?php include "./vistas/inc/NavLateral.php"; ?>
+    <?php include "./vistas/inc/NavLateral.php"; ?>
+    
+     
+    <!-- Page content -->
+    <section >         <div class="content-wrapper">
+        <!-- Content -->
+
+        <div class="container-xxl flex-grow-1 container-p-y">
+
+        <?php 
+        include  "./vistas/inc/Navbar.php"; 
         
-         
-		<!-- Page content -->
-		<section >         <div class="content-wrapper">
-            <!-- Content -->
+        include "$vistas";
+        ?>
+                      
+        <!-- / Content -->
+    </section>
 
-            <div class="container-xxl flex-grow-1 container-p-y">
-
-            <?php 
-			include  "./vistas/inc/Navbar.php"; 
-			
-			include "$vistas";
-			?>
-                          
-            <!-- / Content -->
-		</section>
-
-    <?php 
-    include  "./vistas/inc/LogOut.php";
-	 } 
-	include  "./vistas/inc/Script.php"; ?>
+<?php 
+include  "./vistas/inc/LogOut.php";
+ } 
+include  "./vistas/inc/Script.php"; ?>
 </body>
    <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
